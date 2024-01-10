83b1058032f7e97b5c8869acb77c82e180a42a46
Masznyik Marton <masmar575@logiker.hu>
2024-01-10 10:51:39 +0100

    rovidnap

--- holera/Form1.Designer.cs
+++ holera/Form1.Designer.cs
@@ -41,6 +41,10 @@ namespace holera
             this.label4 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -167,11 +171,52 @@ namespace holera
             this.pictureBox4.TabIndex = 11;
             this.pictureBox4.TabStop = false;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(500, 207);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(500, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(500, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(500, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.pictureBox4);
@@ -210,6 +255,10 @@ namespace holera
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- holera/Form1.cs
+++ holera/Form1.cs
@@ -59,33 +59,47 @@ namespace holera
         {
             public int x;
             public int y;
-            public double tav;
+            public double atlo;
             public pontok(int a, int b)
             {
                 x = a;
                 y = b;
-                tav = Math.Sqrt(x * x + y * y);
+                atlo = Math.Sqrt(x * x + y * y);
             }
             public static bool operator >(pontok a, pontok b)
             {
-                if (a.tav > b.tav)
-                {
+                if (a.atlo > b.atlo)
                     return true;
-                }
                 return false;
             }
             public static bool operator <(pontok a, pontok b)
             {
-                if (a.tav < b.tav)
-                {
+                if (a.atlo < b.atlo)
                     return true;
-                }
                 return false;
+            }
+            public static implicit operator pontok(int a)
+            {
+                return new pontok(a,a);
+            }
+            public static implicit operator pontok(string a)
+            {
+                string[] b = a.Split(';');
+
+                return new pontok(Convert.ToInt32(b[0]),Convert.ToInt32(b[1]));
+            }
+            public static explicit operator double(pontok elso)
+            {
+                return elso.atlo;
             }
         }
 
         pontok vaza = new pontok(9, 10);
         pontok haza = new pontok(10, 15);
+
+        pontok elso = new pontok(10, 2);
+        pontok masodik = new pontok(14, 9);
+
         public Form1()
         {
             InitializeComponent();
@@ -153,19 +167,30 @@ namespace holera
         private void button4_Click(object sender, EventArgs e)
         {
             label4.Text = "";
+
             for (int i = 0; i < t.Length; i++)
-            {
                 Csere(t, i, MaxKiNo(t, i, t.Length));
-            }
 
             for (int i = 0; i < t.Length; i++)
-            {
                 label4.Text += " " + t[i].ToString();
-            }
-
-            // 2 tömb unioja 2 tömb metszete
-
-
         }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+
+            label6.Text = Math.Sqrt(elso.x * elso.x + elso.y * elso.y).ToString();
+            label5.Text = Math.Sqrt(masodik.x * masodik.x + masodik.y * masodik.y).ToString();
+
+            if (elso < masodik)
+                label7.Text = "masodik nagyobb";
+            else
+                label7.Text = "elso nagyobb";
+
+            int x = 20;
+            double y = (double)x;
+
+            // pontok baba = "???";
+        }
+        
     }
 }
